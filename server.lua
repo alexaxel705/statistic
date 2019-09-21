2b2a55065f5bd5e3be18a4c37285c43db2922377
Aleksei <alexaxel705@gmail.com>
2019-09-22 03:17:08 +0700

    Add files via upload

---
+++ server.lua
@@ -0,0 +1,45 @@
+﻿local PlayTime = {}
+function math.round(number, decimals, method)
+    decimals = decimals or 0
+    local factor = 10 ^ decimals
+    if (method == "ceil" or method == "floor") then return math[method](number * factor) / factor
+    else return tonumber(("%."..decimals.."f"):format(number)) end
+end
+
+function ResultGet()
+
+end
+
+function player_Wasted()
+	local x,y,_ = getElementPosition(source)
+	x = math.round(x)
+	y = math.round(y)
+	callRemote("http://109.227.228.4/engine/include/MTA/stats/kill.php", ResultGet, toJSON({x, y}))
+end
+addEventHandler("onPlayerWasted", root, player_Wasted)
+
+
+
+
+
+function Join()
+	PlayTime[source] = getTickCount()
+end
+addEventHandler("onPlayerJoin", getRootElement(), Join)
+
+
+
+function ScriptOn()
+	for theKey,thePlayer in ipairs(getElementsByType("player")) do
+		PlayTime[thePlayer] = getTickCount()
+	end
+end
+addEventHandler("onResourceStart", getResourceRootElement(), ScriptOn)
+
+
+
+function Quit()
+	callRemote("http://109.227.228.4/engine/include/MTA/stats/playtime.php", ResultGet, getTickCount()-PlayTime[source])
+	PlayTime[source] = nil
+end
+addEventHandler("onPlayerQuit", getRootElement(), Quit)
