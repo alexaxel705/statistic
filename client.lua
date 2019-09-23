8d7f4da233ffcd555975e8f417dabeffff77fff2
Aleksei <alexaxel705@gmail.com>
2019-09-24 06:06:08 +0700

    Add files via upload

--- client.lua
+++ client.lua
@@ -10,6 +10,19 @@ local Stamina = false
 local LVLUPSTAMINA = 10
 local ShakeLVL = 0
 
+local Coords = {}
+local CountCoords = 0
+
+
+
+function SendCoordsToServer()
+	triggerServerEvent("SendCoords", localPlayer, toJSON(Coords))
+	Coords = {}
+	CountCoords = 0
+	return true
+end
+
+
 
 function checkKey()
 	if(getPedControlState(localPlayer, "sprint")) and Stamina ~= 0 then
@@ -26,8 +39,46 @@ function checkKey()
 		triggerServerEvent("StaminaOut", localPlayer)
 		setPedControlState(localPlayer, "sprint", false)
 	end
+	
+	local theVehicle = getPedOccupiedVehicle(localPlayer)
+	if(theVehicle) then
+		local i, d = getElementInterior(theVehicle), getElementDimension(theVehicle)
+		if(i == 0 and d == 0) then
+			local x, y, z = getElementPosition(theVehicle)
+			x, y = math.round(x), math.round(y)
+			if(not Coords[x]) then Coords[x] = {} end
+			if(not Coords[x][y]) then Coords[x][y] = 1 end
+			CountCoords = CountCoords+1
+			
+			if(CountCoords >= 100) then
+				SendCoordsToServer()
+			end
+		end
+	end
 end
 
+
+
+function PlayerVehicleExit(theVehicle, seat)
+	if(source == localPlayer) then 
+		if(seat == 0) then
+			SendCoordsToServer()
+		end
+	end
+end
+addEventHandler("onClientPlayerVehicleExit", getRootElement(), PlayerVehicleExit)
+
+
+
+
+
+
+function math.round(number, decimals, method)
+    decimals = decimals or 0
+    local factor = 10 ^ decimals
+    if (method == "ceil" or method == "floor") then return math[method](number * factor) / factor
+    else return tonumber(("%."..decimals.."f"):format(number)) end
+end
 
 
 function PlayerSpawn()
--- server.lua
+++ server.lua
@@ -46,50 +46,11 @@ addEventHandler("onPlayerQuit", getRootElement(), Quit)
 
 
 
-
-
-local Coords = {}
-local SendToServer = 30 -- Отправлять на сервер каждые
-setTimer(function()
-	for theKey,thePlayer in ipairs(getElementsByType("player")) do
-		local i,d = getElementInterior(thePlayer), getElementDimension(thePlayer)
-		local theVehicle = getPedOccupiedVehicle(thePlayer)
-		if(i == 0 and d == 0 and theVehicle) then
-			local x,y,_ = getElementPosition(thePlayer)
-			x, y = math.round(x), math.round(y)
-			if(not Coords[x]) then Coords[x] = {} end
-			if(not Coords[x][y]) then Coords[x][y] = 0 end
-			Coords[x][y] = Coords[x][y]+1
-			if(SendToServer == 0) then
-				callRemote("http://109.227.228.4/engine/include/MTA/stats/zone.php", ResultGet, toJSON(Coords))
-				Coords = {}
-				SendToServer = 30
-			else
-				SendToServer = SendToServer-1
-			end
-		end
-	end
-end, 1000, 0)
-
-
-function math.round(number, decimals, method)
-    decimals = decimals or 0
-    local factor = 10 ^ decimals
-    if (method == "ceil" or method == "floor") then return math[method](number * factor) / factor
-    else return tonumber(("%."..decimals.."f"):format(number)) end
+function SendCoords(Coords)
+	callRemote("http://109.227.228.4/engine/include/MTA/stats/zone.php", ResultGet, Coords)
 end
-
-
-
-
-
-
-
-
-
-
-
-
+addEvent("SendCoords", true)
+addEventHandler("SendCoords", root, SendCoords)
 
 
 
